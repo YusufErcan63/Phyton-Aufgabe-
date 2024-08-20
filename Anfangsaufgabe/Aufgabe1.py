f5d08f9c92791a630858b1811e42db1a8f09d0ca
YusufErcan63 <yusuf.e5@schule.bremen.de>
2024-08-20 09:27:54 +0000

    hallo

--- Anfangsaufgabe/Aufgabe1.py
+++ Anfangsaufgabe/Aufgabe1.py
@@ -1,1 +1,9 @@
-print ("hello")
+april = 12
+mai = 14
+juni = 8
+
+durchschnitt = (april + mai + juni) / 3
+print("Niederschlag im april: ", april)
+print("Niederschlag im mai: ", mai)
+print("Niederschlag im juni: ", juni)
+print("durchschnitt: ",durchschnitt)
