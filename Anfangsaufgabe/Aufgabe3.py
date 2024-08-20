9120cca3405527d8220b49fe78bf1792ac3655c9
YusufErcan63 <yusuf.e5@schule.bremen.de>
2024-08-20 13:04:16 +0000

    ghg

---
+++ Anfangsaufgabe/Aufgabe3.py
@@ -0,0 +1,4 @@
+celcius= 10
+Fahrenheit= 9/5 * celcius + 32
+
+print ("%s Grad Celcius sind %d Grad in Fahrenheit" %(celcius, Fahrenheit))
---
+++ Anfangsaufgabe/Aufgabe4.py
@@ -0,0 +1,4 @@
+from numpy import sqrt
+g= 9.81 
+t= 2 * 80 / g
+print (sqrt(t)*g, "m/s," , sqrt (t)*g*3.6, "km/h")
